2ed14b409cc0973b8c15d74ed33543bd00c688f5
dmitchsplunk <derekmitchell@splunk.com>
2024-12-17 12:01:07 -0800

    initial commit

---
+++ init_opentelemetry.py
@@ -0,0 +1,36 @@
+import os 
+import logging
+from opentelemetry import trace, _logs
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
+
+def init_opentelemetry(): 
+
+    if 'OTEL_SERVICE_NAME' not in os.environ: 
+        raise Exception('The OTEL_SERVICE_NAME environment variable must be set')
+
+    if 'OTEL_EXPORTER_OTLP_ENDPOINT' not in os.environ: 
+        raise Exception('The OTEL_EXPORTER_OTLP_ENDPOINT environment variable must be set')
+    
+    if 'OTEL_RESOURCE_ATTRIBUTES' not in os.environ: 
+        raise Exception('The OTEL_RESOURCE_ATTRIBUTES environment variable must be set')
+
+    traceProvider = TracerProvider()
+    processor = BatchSpanProcessor(OTLPSpanExporter())
+    traceProvider.add_span_processor(processor)
+    trace.set_tracer_provider(traceProvider)
+
+    _logs.set_logger_provider(
+        LoggerProvider()
+    )
+    logging.getLogger().addHandler(
+        LoggingHandler(
+            logger_provider=_logs.get_logger_provider().add_log_record_processor(
+                BatchLogRecordProcessor(OTLPLogExporter())
+            )
+        )
+    )
